53f2df42dadc62b3acd8a6fcfe92f32239cf9742
Lenovo <asadbek.polatov.02@mail.ru>
2022-08-11 08:46:48 +0500

    start qoshildi

--- index.php
+++ index.php
@@ -7,6 +7,9 @@ $telegram = new Telegram('1210681190:AAFpNO1ochbktqThf4vewibUEESigM04oiA');
 $chat_id = $telegram->ChatID();
 //$chat_id = 1210681190;
 $text = $telegram->Text();
+
+if ($text == '/start')
+    $content = array('chat_id' => $chat_id, 'text' => 'Assalomu alaykum botimizga xush kelibsiz!');
 $content = array('chat_id' => $chat_id, 'text' => $text);
 $telegram->sendMessage($content);
 // var_dump($content);
