9c55754bf61e276de365f01e8c6d28a1c8801931
Lenovo <asadbek.polatov.02@mail.ru>
2022-08-11 10:54:09 +0500

    weather

--- index.php
+++ index.php
@@ -6,6 +6,18 @@ $chat_id = $telegram->ChatID();
 $text = $telegram->Text();
 $f = '';
 if ($text == '/start') {
+    showMenu();
+} else if ($text == "🌍 Weather") {
+    $content = array('chat_id' => $chat_id, 'text' => "Qayerniki kerak");
+    $telegram->sendMessage($content);
+    $f = $text;
+} else {
+    showWeather();
+}
+
+function showMenu()
+{
+    global $telegram, $chat_id;
     $option = array(
         array($telegram->buildKeyboardButton("🌍 Weather")),
 //        array($telegram->buildKeyboardButton("❤️ Love", true, false)),
@@ -15,17 +27,25 @@ if ($text == '/start') {
     $telegram->sendMessage($content);
     $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => 'Tugmalardan birini tanlang');
     $telegram->sendMessage($content);
-} else if ($text == "🌍 Weather") {
-    $content = array('chat_id' => $chat_id, 'text' => "Qayerniki kerak");
-    $telegram->sendMessage($content);
-    $f = $text;
-} else {
+}
+
+function showWeather()
+{
+    global $telegram, $text, $chat_id;
     $q = $text;
     $key = "a85c63aee77341ee89b50718223004";
     $aqi = "yes";
     $api_url = "http://api.weatherapi.com/v1/current.json?key=" . $key . "&aqi=" . $aqi . "&q=" . $q;
     $data = json_decode(file_get_contents($api_url), true);
-    $content = array('chat_id' => $chat_id, 'text' => "Harorat " . $data['current']['temp_c']);
+    $show = "<img src='".$data['current']['condition']['icon']."' ><br>".
+            "📍 Name: ".$data['location']['name']."<br>".
+            "📍 Region: ".$data['location']['region']."<br>".
+            "📍 Country: ".$data['location']['country']."<br>".
+            "🌡 Temperature: ".$data['current']['temp_c']."<br>.";
+            "🌪 Wind: ".$data['current']['wind_mph']."<br>.";
+            "💧 Humidity: ".$data['current']['humidity']."<br>.";
+            "🕔 Time: ".$data['current']['last_updated']."<br>";
+    $content = array('chat_id' => $chat_id, 'text' => $show);
     $telegram->sendMessage($content);
 }
 
