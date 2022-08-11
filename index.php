28f7f6f98df5aee1b6a523cd97bf226191995c61
Lenovo <asadbek.polatov.02@mail.ru>
2022-08-11 09:55:00 +0500

    api

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
+$key = "a85c63aee77341ee89b50718223004";
+$aqi = "yes";
+//if (isset($_GET['location'])){
+//    $q = $_GET['location'];
+//} else
+    $q = "Pitnak";
+$api_url = "http://api.weatherapi.com/v1/current.json?key=".$key."&aqi=".$aqi."&q=".$q;
+$data = json_decode(file_get_contents($api_url), true);
+echo $data;
 include 'Telegram.php';
 
 $telegram = new Telegram('1210681190:AAFpNO1ochbktqThf4vewibUEESigM04oiA');
@@ -18,8 +27,8 @@ if ($text == '/start'){
     $telegram->sendMessage($content);
     $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb ,'text' => 'Tugmalardan birini tanlang');
     $telegram->sendMessage($content);
-
 }
+
 //$content = array('chat_id' => $chat_id, 'text' => $text);
 //$telegram->sendMessage($content);
 ?>
