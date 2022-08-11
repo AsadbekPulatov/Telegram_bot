ea9708e7fe3ac4d00457eb534d7f6669453073ed
Lenovo <asadbek.polatov.02@mail.ru>
2022-08-11 11:03:13 +0500

    weather

--- index.php
+++ index.php
@@ -37,14 +37,15 @@ function showWeather()
     $aqi = "yes";
     $api_url = "http://api.weatherapi.com/v1/current.json?key=" . $key . "&aqi=" . $aqi . "&q=" . $q;
     $data = json_decode(file_get_contents($api_url), true);
-    $show = "<img src='".$data['current']['condition']['icon']."' />".
-            "<p> 📍 Name: ".$data['location']['name']."</p>".
-            "<p> 📍 Region: ".$data['location']['region']."</p>".
-            "<p> 📍 Country: ".$data['location']['country']."</p>".
-            "<p> 🌡 Temperature: ".$data['current']['temp_c']."</p>.";
-            "<p> 🌪 Wind: ".$data['current']['wind_mph']."</p>.";
-            "<p> 💧 Humidity: ".$data['current']['humidity']."</p>.";
-            "<p> 🕔 Time: ".$data['current']['last_updated']."</p>";
+    $show = "<img src='" . $data['current']['condition']['icon'] . "' />" .
+        "<p> 📍 Name: " . $data['location']['name'] . "</p>" .
+        "<p> 📍 Region: " . $data['location']['region'] . "</p>" .
+        "<p> 📍 Country: " . $data['location']['country'] . "</p>" .
+        "<p> 🌡 Temperature: " . $data['current']['temp_c'] . "</p>.";
+    "<p> 🌪 Wind: " . $data['current']['wind_mph'] . "</p>.";
+    "<p> 💧 Humidity: " . $data['current']['humidity'] . "</p>.";
+    "<p> 🕔 Time: " . $data['current']['last_updated'] . "</p>";
+    var_dump($show);
     $content = array('chat_id' => $chat_id, 'text' => $show, 'parse_mode' => 'html');
     $telegram->sendMessage($content);
 }
